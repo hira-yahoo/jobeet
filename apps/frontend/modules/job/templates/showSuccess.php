8a8e56363163d433f26021a731ef387dbf8be735
Alice <alice@example.com>
2015-06-14 16:39:29 +0900

    define titles respectively by using a helper.

--- apps/frontend/modules/job/templates/showSuccess.php
+++ apps/frontend/modules/job/templates/showSuccess.php
@@ -1,3 +1,7 @@
+<?php slot('title') ?>
+  <?php echo sprintf('%s is looking for a %s', $job->getCompany(), $job->getPosition()) ?>
+<?php end_slot() ?>
+
 <?php use_stylesheet('job.css') ?>
 <?php use_helper('Text') ?>
 
